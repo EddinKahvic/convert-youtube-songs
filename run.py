2d7a11989eb8607896abc8fdc9ac5fc50c6b0230
b21eddka <b21eddka@student.his.se>
2024-01-14 22:53:59 +0100

    Retrieves the songs from playlist with thier true title

--- run.py
+++ run.py
@@ -2,10 +2,24 @@ from youtube_client import YoutubeClient
 
 def run():
     # Get the playlist list from YouTube
-    youtube_client = YoutubeClient('./credentials/client_secret_1061033002100-2pj465cq3fhsspi0a85d8jh5cgp2hs4p.apps.googleusercontent.com.json')   
+    youtube_client = YoutubeClient('./credentials/client_secret.json')   
     playlists = youtube_client.get_playlists()
     
+    # Choose what playlist to extract music from
+    for index, playlist in enumerate(playlists):
+        print(f"{index}: {playlist.title}")
+    choice = int(input("Enter the playlist: "))
+    chosen_playlist = playlists[choice]
+    print(f"You selected: {chosen_playlist.title}")
     
+    # For each video in playlist, get track and artist
+    songs = youtube_client.get_videos(chosen_playlist.id)
+    print(f"Attempting to add {len(songs)} songs...")
+    for song in songs:
+        print(song.track)
+
+    
+        
 
 
 
--- youtube_client.py
+++ youtube_client.py
@@ -3,12 +3,11 @@ import os
 import google_auth_oauthlib.flow
 import googleapiclient.discovery
 import yt_dlp
+import re
 
-scopes = ["https://www.googleapis.com/auth/youtube.readonly"]
 
 class Song(object):
-    def __init__(self, artist, track):
-        self.artist = artist
+    def __init__(self, track):
         self.track = track
 
 class Playlist(object):
@@ -16,8 +15,11 @@ class Playlist(object):
         self.id = id
         self.title = title
 
+
 class YoutubeClient(object):
     def __init__(self, client_secrets_file_location):
+        scopes = ["https://www.googleapis.com/auth/youtube.readonly"]
+        
         # Disable OAuthlib's HTTPS verification when running locally.
         # *DO NOT* leave this option enabled in production.
         os.environ["OAUTHLIB_INSECURE_TRANSPORT"] = "1"
@@ -28,7 +30,7 @@ class YoutubeClient(object):
         # Get credentials and create an API client
         flow = google_auth_oauthlib.flow.InstalledAppFlow.from_client_secrets_file(
             client_secrets_file_location, scopes)
-        credentials = flow.run_console()
+        credentials = flow.run_local_server()
         youtube_client = googleapiclient.discovery.build(
             api_service_name, api_version, credentials=credentials)
         
@@ -36,7 +38,7 @@ class YoutubeClient(object):
 
 
     def get_playlists(self):
-        request = self.playlists().list(
+        request = self.youtube_client.playlists().list(
             part="id, snippet",
             maxResults=20,
             mine=True
@@ -46,9 +48,9 @@ class YoutubeClient(object):
         playlists = [Playlist(item['id'], item['snippet']['title']) for item in response['items']]
         return playlists 
 
-
+    # Finish this after basic functionality is done.
     def get_liked_videos(self):
-        request = self.videos().list(
+        request = self.youtube_client.videos().list(
             part="id, snippet",
             myRating="like"
         )
@@ -58,8 +60,9 @@ class YoutubeClient(object):
 
 
     def get_videos(self, playlist_id):
-        request = self.playlistItems().list(
+        request = self.youtube_client.playlistItems().list(
             part="id, snippet",
+            maxResults=500,
             playlistId=playlist_id
         )
         response = request.execute()
@@ -67,20 +70,25 @@ class YoutubeClient(object):
         songs = []
         
         for item in response['items']:
-            video_id = item['snippet']['resrouceId']['videoId']
-            artist, track = get_artist_and_track(video_id)
-            if artist and track:
-                songs.append(Song(artist, track))
-                return songs
+            video_id = item['snippet']['resourceId']['videoId']
+            track = self.get_track(video_id)
+            if track:
+                songs.append(Song(track))
+                
+        return songs
 
 
-    def get_artist_and_track(self, video_id):
+    def get_track(self, video_id):
         youtube_url = f"https://www.youtube.com/watch?v={video_id}"
         
-        video = yt_dlp.YoutubeDL({'quiet': True}).extract_info(
-            youtube_url, download=False
-        )
-        
-        artist = video['artist']
-        track = video['track']
-        return artist, track
+        ydl_opts = {'quiet': True}
+        with yt_dlp.YoutubeDL(ydl_opts) as ydl:
+            info = ydl.extract_info(youtube_url, download=False)
+            print(info['categories'])
+            # filter, only 'Music' is allowed
+            if 'Music' in info['categories']:
+                
+                # Extract true title - get rid of: [], (), whitespace
+                title = info['title']
+                true_title = "".join(re.split("\(|\)|\[|\]|\'|\'", title)[::2]).strip()
+                return true_title
